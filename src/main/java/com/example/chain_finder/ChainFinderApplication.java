aa5a82415fe701b2ba92720063ce7de5aa158992
ASUS <ms0565119@hotmail.com>
2023-05-04 22:57:43 +0800

    創建gradle

---
+++ src/main/java/com/example/chain_finder/ChainFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.chain_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainFinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChainFinderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/chain_finder/ChainFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chain_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainFinderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
